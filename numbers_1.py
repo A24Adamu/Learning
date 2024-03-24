48b861a6f8804be8527776a1af39b5159d4e4ed5
A24Adamu <a24adamu@gmail.com>
2024-03-24 02:22:51 +0000

    Remove all comments for cleaner code

---
+++ numbers_1.py
@@ -0,0 +1,17 @@
+
+number1 = int(input("Enter first integer: ")) 
+number2 = int(input("Enter second integer: "))
+number3 = int(input("Enter third integer: "))
+
+
+sum = number1 + number2 + number3
+
+
+minus = number1 - number2
+
+multiply = number3 * number1
+
+
+division = sum/number3
+
+print("First integer = {}\nSecond integer = {}\nThird integer = {}\nSum of the three integers = {}\nFirst integer - second integer = {}\nThe third integer x the first integer = {}\nSum of first three integers/the third integer = {}".format(number1, number2, number3, sum, minus, multiply, division))
